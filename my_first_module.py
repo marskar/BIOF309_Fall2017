205a2e057677288a910ad487f7d67f9e11e2351b
anuhanovic <antejn@gmail.com>
2017-03-16 11:44:20 -0400

    Add files via upload

---
+++ my_first_module.py
@@ -0,0 +1,6 @@
+def countdown_from_y_to_x(y, x):
+    """greatest function ever"""
+    n=y
+    while(n>=x):
+        print(n)
+        n = n-1 #n -= 1
---
+++ runit.py
@@ -0,0 +1,2 @@
+import 'my_first_module.copy'
+my_first_module.countdown_from_y_to_x(25, 20)
